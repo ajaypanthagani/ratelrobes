b6814aa773d6c32d42b15af11541257ce6cebf1f
ajaypanthagani <ajay.panthagani@ninjavan.co>
2024-06-19 19:01:39 +0530

    make animation better

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Select the logo element
     const logo = document.querySelector('.logo');
+    const caption = document.querySelector('.caption')
     
     // Detect when shine animation ends
     logo.addEventListener('animationend', function(event) {
         // Check if the ended animation is 'shine'
-        if (event.animationName === 'shine') {
+        if (event.animationName === 'shine-right') {
             // Select the img element inside .logo
             const img = logo.querySelector('img');
+            const text = caption.querySelector('h2')
             // Make the img visible and fade it in
             img.style.visibility = 'visible';
             img.style.opacity = '1';
+            text.style.visibility = 'visible';
+            text.style.opacity = '1';
         }
     });
 });
